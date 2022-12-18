126af53c9cca95e433285f060f9c29a750270e65
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2022-12-17 21:51:48 -0600

    Update +layout.server.ts

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,5 @@
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = () => {
-	return { env: import.meta.env };
+	return { env: import.meta.env, analyticsId: process.env.VERCEL_ANALYTICS_ID };
 };
