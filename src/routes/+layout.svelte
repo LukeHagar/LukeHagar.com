4b3c7af5f0afe65044e18eabb9f9dfd8f7c48dc4
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2022-12-17 20:46:50 -0600

    Modernization to celebrate 1.0, and my new Team :)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,53 +1,18 @@
 <script>
-	import Header from './Header.svelte';
-	import './styles.css';
+  // import "@skeletonlabs/skeleton/themes/theme-skeleton.css";
+  import "$lib/theme-luke.css";
+  import "@skeletonlabs/skeleton/styles/all.css";
+  import "../app.postcss";
+  import { AppShell } from "@skeletonlabs/skeleton";
+  import Header from "$lib/Header.svelte";
 </script>
 
-<div class="app">
-	<Header />
-
-	<main>
-		<slot />
-	</main>
-
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
-</div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
-</style>
+<!-- App Shell -->
+<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
+  <svelte:fragment slot="header">
+    <!-- App Bar -->
+    <Header />
+  </svelte:fragment>
+  <!-- Page Route Content -->
+  <slot />
+</AppShell>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,63 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+  import { Avatar, GradientHeading } from "@skeletonlabs/skeleton";
+  import Particles from "svelte-particles";
+  import { loadFull } from "tsparticles";
+
+  let particlesUrl = "http://foo.bar/particles.json";
+
+  let particlesConfig = {
+    particles: {
+      color: {
+        value: "#ffff",
+      },
+      links: {
+        enable: true,
+        color: "#ffff",
+      },
+      move: {
+        enable: true,
+      },
+    },
+  };
+
+  let onParticlesLoaded = (event) => {
+    const particlesContainer = event.detail.particles;
+
+    // you can use particlesContainer to call all the Container class
+    // (from the core library) methods like play, pause, refresh, start, stop
+  };
+
+  let particlesInit = async (engine) => {
+    // you can use main to customize the tsParticles instance adding presets or custom shapes
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+  };
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
+<div class="z-0">
+  <Particles
+    id="tsparticles"
+    options={particlesConfig}
+    on:particlesLoaded={onParticlesLoaded}
+    {particlesInit}
+  />
+</div>
+<div class="z-50 mx-auto flex h-full items-center justify-center">
+  <div class="card card-glass z-50 space-y-10 p-10">
+    <span class="flex flex-row gap-2">
+      <p class="unstyled text-6xl font-bold !text-white">Hi, I'm</p>
+      <GradientHeading
+        class="text-6xl"
+        tag="h1"
+        direction="bg-gradient-to-r"
+        from="from-primary-500"
+        to="to-surface-500">Luke</GradientHeading
+      >
+    </span>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+    <div class="flex justify-center space-x-2" />
+    <div class="space-y-2" />
+    <Avatar src="/Luke.png" width="w-48" class="mx-auto" />
+  </div>
+</div>
