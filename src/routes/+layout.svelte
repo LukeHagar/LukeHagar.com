d1e51bc5fd66d9b8e4933a93ad83a26c63e6504b
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2022-12-17 21:19:49 -0600

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
   import { AppShell } from "@skeletonlabs/skeleton";
   import Header from "$lib/Header.svelte";
   import { inject } from "@vercel/analytics";
-  import { VERCEL_ANALYTICS_ID } from "$/env/static/public";
+  import { VERCEL_ANALYTICS_ID } from "$env/static/public";
   console.log(import.meta.env.VERCEL_ANALYTICS_ID);
   console.log(VERCEL_ANALYTICS_ID);
 
