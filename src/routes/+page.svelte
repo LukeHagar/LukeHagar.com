412cefd366432f6db60df076f8d800ebb413f3f0
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2022-12-17 21:16:42 -0600

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,7 @@
   import Particles from "svelte-particles";
   import { loadFull } from "tsparticles";
 
-  let particlesUrl = "http://foo.bar/particles.json";
-
-  let particlesConfig = {
+  const particlesConfig = {
     particles: {
       color: {
         value: "#ffff",
@@ -20,14 +18,14 @@
     },
   };
 
-  let onParticlesLoaded = (event) => {
+  const onParticlesLoaded = (event) => {
     const particlesContainer = event.detail.particles;
 
     // you can use particlesContainer to call all the Container class
     // (from the core library) methods like play, pause, refresh, start, stop
   };
 
-  let particlesInit = async (engine) => {
+  const particlesInit = async (engine) => {
     // you can use main to customize the tsParticles instance adding presets or custom shapes
     // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
     // starting from v2 you can add only the features you need reducing the bundle size
