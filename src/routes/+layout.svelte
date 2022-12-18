20e9f11d0993835438d7057d7c4e5fa9877afa81
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2022-12-17 21:22:09 -0600

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,13 +6,10 @@
   import { AppShell } from "@skeletonlabs/skeleton";
   import Header from "$lib/Header.svelte";
   import { inject } from "@vercel/analytics";
-  import {
-    VERCEL_ANALYTICS_ID,
-    VITE_VERCEL_ANALYTICS_ID,
-  } from "$env/static/public";
+  import { VITE_VERCEL_ANALYTICS_ID } from "$env/static/public";
   console.log(import.meta.env.VERCEL_ANALYTICS_ID);
-  console.log(VERCEL_ANALYTICS_ID);
   console.log(VITE_VERCEL_ANALYTICS_ID);
+  console.log(import.meta.env.VITE_VERCEL_ANALYTICS_ID);
 
   inject();
 </script>
