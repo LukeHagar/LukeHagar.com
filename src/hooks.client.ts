fbe886df53f33bff4ced1eafdadf1d073cc3c81f
Luke Hagar <lukeslakemail@gmail.com>
2024-02-07 11:49:25 -0600

    added sentry environment

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,24 +1,24 @@
-
 // This file is used to initialize Sentry on the client side.
 import { dev } from '$app/environment';
 import * as Sentry from '@sentry/sveltekit';
 
-
 Sentry.init({
-  dsn: 'https://6b2d8c5156a83705bc7ffb4f0d16ba5f@sentry.plygrnd.org/2',
+	environment: dev ? 'development' : 'production',
 
-  tracesSampleRate: 1.0,
+	dsn: 'https://132cfeda18443f305824133b96e8bc0e@sentry.plygrnd.org/2',
 
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: dev ? 1.0 : 0.1,
+	tracesSampleRate: 1.0,
 
-  // If the entire session is not sampled, use the below sample rate to sample
-  // sessions when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
-  
-  // If you don't want to use Session Replay, just remove the line below:
-  integrations: [new Sentry.Replay()],
+	// This sets the sample rate to be 10%. You may want this to be 100% while
+	// in development and sample at a lower rate in production
+	replaysSessionSampleRate: dev ? 1.0 : 0.1,
+
+	// If the entire session is not sampled, use the below sample rate to sample
+	// sessions when an error occurs.
+	replaysOnErrorSampleRate: 1.0,
+
+	// If you don't want to use Session Replay, just remove the line below:
+	integrations: [new Sentry.Replay()]
 });
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
