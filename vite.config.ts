b123fcaec8b020368e3fa6aba5561a419f9b8855
Luke Hagar <lukeslakemail@gmail.com>
2024-05-20 11:20:19 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
-import { sentrySvelteKit } from '@sentry/sveltekit';
+
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
-		sentrySvelteKit({
-			sourceMapsUploadOptions: {
-				org: 'sentry',
-				project: 'lukehagar',
-				url: 'https://sentry.plygrnd.org',
-				authToken: process.env.SENTRY_AUTH_TOKEN
-			}
-		}),
 		sveltekit()
 	],
 	ssr: {
